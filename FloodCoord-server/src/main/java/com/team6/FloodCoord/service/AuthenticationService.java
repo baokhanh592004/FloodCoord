43ab6d565e7b287d9f177fb3600139cc7760d8b1
LuongMike <luongdvse181578@fpt.edu.vn>
2026-01-28 20:11:20 +0700

    feat(auth): add change, forgot and reset password endpoints

--- FloodCoord-server/src/main/java/com/team6/FloodCoord/service/AuthenticationService.java
+++ FloodCoord-server/src/main/java/com/team6/FloodCoord/service/AuthenticationService.java
@@ -1,7 +1,10 @@
 package com.team6.floodcoord.service;
 
 import com.nimbusds.jose.JOSEException;
+import com.team6.floodcoord.dto.request.ChangePasswordRequest;
+import com.team6.floodcoord.dto.request.ForgotPasswordRequest;
 import com.team6.floodcoord.dto.request.LoginRequest;
+import com.team6.floodcoord.dto.request.ResetPasswordRequest;
 import com.team6.floodcoord.dto.response.LoginResponse;
 
 import java.text.ParseException;
@@ -10,4 +13,7 @@ public interface AuthenticationService {
     void logout(String token) throws ParseException;
     LoginResponse login(LoginRequest request);
     LoginResponse refreshToken(String refreshToken) throws ParseException, JOSEException;
+    void changePassword(Long userId, ChangePasswordRequest request);
+    void forgotPassword(ForgotPasswordRequest request);
+    void resetPassword(ResetPasswordRequest request);
 }
