0f68fb2dadbf4bb7bc5ac22d45ee3355edb0f686
LuongMike <luongdvse181578@fpt.edu.vn>
2026-02-03 09:44:25 +0700

    refactor: update RoleRepository and PasswordResetTokenRepository

--- FloodCoord-server/src/main/java/com/team6/FloodCoord/repository/PasswordResetTokenRepository.java
+++ FloodCoord-server/src/main/java/com/team6/FloodCoord/repository/PasswordResetTokenRepository.java
@@ -5,10 +5,12 @@ import com.team6.floodcoord.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
     Optional<PasswordResetToken> findByUser(User user);
