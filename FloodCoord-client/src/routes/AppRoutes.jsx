64dbfbb9e207947ee7cc4d5a70855fafc22de4c3
trongnt <ntantrong9.2@gmail.com>
2026-02-05 19:45:30 +0700

    chore: add route for coordinator screen

--- FloodCoord-client/src/routes/AppRoutes.jsx
+++ FloodCoord-client/src/routes/AppRoutes.jsx
@@ -6,6 +6,12 @@ import LoginPage from '../pages/auth/LoginPage'
 import RequestRescuePage from '../pages/rescue/RequestRescuePage'
 import ForgotPasswordPage from '../pages/auth/ForgotPasswordPage'
 import ResetPasswordPage from '../pages/auth/ResetPasswordPage'
+import PrivateRoute from './PrivateRoute'
+import CoordinatorLayout from '../layouts/CoordinatorLayout'
+import CoordinatorDashboard from '../pages/coordinator/CoordinatorDashboard'
+import RequestQueue from '../pages/coordinator/RequestQueue'
+import AssignTeams from '../pages/coordinator/AssignTeams'
+import Operations from '../pages/coordinator/Operations'
 
 export default function AppRoutes() {
   return (
@@ -18,6 +24,20 @@ export default function AppRoutes() {
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/request-rescue" element={<RequestRescuePage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
+      </Route>
+
+      <Route
+        path="/coordinator"
+        element={
+          <PrivateRoute>
+            <CoordinatorLayout />
+          </PrivateRoute>
+        }
+      >
+        <Route path="dashboard" element={<CoordinatorDashboard />} />
+        <Route path="requests" element={<RequestQueue />} />
+        <Route path="assign-teams" element={<AssignTeams />} />
+        <Route path="operations" element={<Operations />} />
       </Route>
 
       <Route
