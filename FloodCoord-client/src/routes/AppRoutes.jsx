b4a5192ca6b1a48d9bada7fda99afb0e5882cb54
khiem <khiemphamk2004.com>
2026-02-05 18:48:29 +0700

    HomePage

--- FloodCoord-client/src/routes/AppRoutes.jsx
+++ FloodCoord-client/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from '../pages/Home'
+import HomePage from '../pages/HomePage'
 import { Route, Routes } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import LoginPage from '../pages/auth/LoginPage'
@@ -7,13 +7,14 @@ import RequestRescuePage from '../pages/rescue/RequestRescuePage'
 import ForgotPasswordPage from '../pages/auth/ForgotPasswordPage'
 import ResetPasswordPage from '../pages/auth/ResetPasswordPage'
 
+
 export default function AppRoutes() {
   return (
     <Routes>
 
       <Route element={<MainLayout />}>
         {/* Route công khai - ai cũng xem được */}
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
         <Route path="/request-rescue" element={<RequestRescuePage />} />
