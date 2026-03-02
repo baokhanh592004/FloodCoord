10ec7648cda36a8cd6412d80079658e6ebc01d95
LuongMike <luongmike170304@gmail.com>
2026-03-03 00:17:05 +0700

    feat(user-profile): implement profile management and update functionality

---
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/dto/request/ProfileUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.team6.floodcoord.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProfileUpdateRequest {
+    private String fullName;
+    private String phoneNumber;
+}
