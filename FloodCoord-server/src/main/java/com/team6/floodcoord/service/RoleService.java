7fe67c623efad237fe6be4de61c6c8ecbdd6a1d1
LuongMike <luongmike170304@gmail.com>
2026-03-08 11:21:43 +0700

    feat(role): implement role management APIs and service layer

---
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/service/RoleService.java
@@ -0,0 +1,9 @@
+package com.team6.floodcoord.service;
+
+import com.team6.floodcoord.dto.response.RoleResponse;
+
+import java.util.List;
+
+public interface RoleService {
+    List<RoleResponse> getAllRoles();
+}
