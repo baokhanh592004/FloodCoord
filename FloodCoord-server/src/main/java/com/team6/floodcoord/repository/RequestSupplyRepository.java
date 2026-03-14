436976f0b81319ed02df53c65e5fcc49b7074406
LuongMike <luongmike170304@gmail.com>
2026-03-15 03:21:41 +0700

    feat(repository): add incident repository and update supply queries

--- FloodCoord-server/src/main/java/com/team6/floodcoord/repository/RequestSupplyRepository.java
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/repository/RequestSupplyRepository.java
@@ -1,8 +1,10 @@
 package com.team6.floodcoord.repository;
 
 import com.team6.floodcoord.model.RequestSupply;
+import com.team6.floodcoord.model.RescueRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,4 +13,6 @@ public interface RequestSupplyRepository extends JpaRepository<RequestSupply, Lo
     Optional<RequestSupply> findByRequest_RequestIdAndSupply_Id(
             UUID requestId,
             Long supplyId);
+
+    List<RequestSupply> findByRequest(RescueRequest rescueRequest);
 }
