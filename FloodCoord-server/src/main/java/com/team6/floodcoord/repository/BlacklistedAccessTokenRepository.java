45733ad576794e4bfd3c8791968fbbf59a9858e4
mlong05 <minhlongwa3@gmail.com>
2026-03-23 14:47:09 +0700

    fix:resolve Render deployment issues (change type from CRUDRepository to KeyValueRepository)

--- FloodCoord-server/src/main/java/com/team6/floodcoord/repository/BlacklistedAccessTokenRepository.java
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/repository/BlacklistedAccessTokenRepository.java
@@ -1,9 +1,10 @@
 package com.team6.floodcoord.repository;
 
 import com.team6.floodcoord.model.BlackListedAccessToken;
+import org.springframework.data.keyvalue.repository.KeyValueRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlacklistedAccessTokenRepository extends CrudRepository<BlackListedAccessToken, String> {
+public interface BlacklistedAccessTokenRepository extends KeyValueRepository<BlackListedAccessToken, String> {
 }
