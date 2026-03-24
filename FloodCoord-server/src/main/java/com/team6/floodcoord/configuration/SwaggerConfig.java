cce6a4b5fa01fbe3f832dd81ce878089ecf9f529
mlong05 <minhlongwa3@gmail.com>
2026-03-24 08:59:51 +0700

    fix: adjust some config for swagger to run on Render

--- FloodCoord-server/src/main/java/com/team6/floodcoord/configuration/SwaggerConfig.java
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/configuration/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.team6.floodcoord.configuration;
 
+
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +21,10 @@ public class SwaggerConfig {
                         .title("FloodCoord API")
                         .version("1.0")
                         .description("API documentation for FloodCoord system"))
+
+                // 🔥 THÊM DÒNG NÀY
+                .addServersItem(new Server().url("https://floodcoord.onrender.com"))
+
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .schemaRequirement(securitySchemeName,
                         new SecurityScheme()
