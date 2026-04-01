7baade406d04c307e87a7fe4184b27695bb40fbe
LuongMike <luongmike170304@gmail.com>
2026-04-01 11:10:25 +0700

    feat(controller): add pagination to supply inventory endpoint

--- FloodCoord-server/src/main/java/com/team6/floodcoord/controller/SupplyController.java
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/controller/SupplyController.java
@@ -6,6 +6,9 @@ import com.team6.floodcoord.service.SupplyService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -41,8 +44,10 @@ public class SupplyController {
 
     @GetMapping
     @Operation(summary = "Xem danh sách tồn kho")
-    public ResponseEntity<List<SupplyResponse>> getAllSupplies() {
-        return ResponseEntity.ok(supplyService.getAllSupplies());
+    public ResponseEntity<Page<SupplyResponse>> getAllSupplies(
+            @PageableDefault(page = 0, size = 10) Pageable pageable
+    ) {
+        return ResponseEntity.ok(supplyService.getAllSupplies(pageable));
     }
 
     @GetMapping("/{id}")
