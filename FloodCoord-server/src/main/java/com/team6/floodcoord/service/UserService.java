8b30e6832cef66d8d24e61f5a9e6638278b470bb
LuongMike <luongmike170304@gmail.com>
2026-04-01 11:09:15 +0700

    feat(service): implement pagination for user retrieval

--- FloodCoord-server/src/main/java/com/team6/floodcoord/service/UserService.java
+++ FloodCoord-server/src/main/java/com/team6/floodcoord/service/UserService.java
@@ -5,6 +5,8 @@ import com.team6.floodcoord.dto.request.UserRequest;
 import com.team6.floodcoord.dto.request.UserUpdateRequest;
 import com.team6.floodcoord.dto.response.UserResponse;
 import com.team6.floodcoord.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -12,7 +14,7 @@ import java.util.List;
 public interface UserService {
     UserResponse createUser(UserRequest request);
     public UserResponse updateUser(Long id, UserUpdateRequest request);
-    public List<UserResponse> getAllUsers();
+    Page<UserResponse> getAllUsers(Pageable pageable);
     public void deleteUser(Long id);
     public UserResponse getUserById(Long id);
     UserResponse getMyProfile(User currentUser);
